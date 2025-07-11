12faf43257517ab27ff66a232424ff3da66e85cb
CharlieNielson <charlienielson@mail.weber.edu>
2025-07-11 17:03:33 -0600

    Create ISBN Calculator.cpp

---
+++ ISBN Calculator.cpp
@@ -0,0 +1,19 @@
+include <iostream>
+using namespace std;
+
+int main() {//Calculates c for an ISBN number. I don't think I will ever use this again, but I lazy.
+    int calc = 0;
+    int end = 0;
+    cout << "Put in the ISBN: ";
+    cin >> calc;
+    int len = 9;
+    for (int i = 1; i <= len; i++) {
+        end += (calc % 10) * (len + 1 - i);
+        calc -= (calc % 10);
+        calc = calc / 10;
+    }
+    end = end % 11;
+    cout << "C: " << end << endl;
+
+    return 17;
+}
